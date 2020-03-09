cd18c1a2ad0bcec0089492ee27b99f2186c3e8f3
Aty <aty99@yahoo.com>
2020-03-09 23:18:16 +0200

    Initial Commit

---
+++ Side Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Side Menu
+//
+//  Created by Mohamed Abd el Aty on 3/9/20.
+//  Copyright © 2020 Aty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
